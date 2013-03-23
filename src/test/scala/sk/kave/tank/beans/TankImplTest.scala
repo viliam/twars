e6fac0736037c3aad410e4d7c937fcc24fd21117
Viliam Kois <viliam.kois@sfb.sk>
2013-03-24 00:36:40 +0100

    tank change direction test

--- src/test/scala/sk/kave/tank/beans/TankImplTest.scala
+++ src/test/scala/sk/kave/tank/beans/TankImplTest.scala
@@ -2,8 +2,11 @@ package sk.kave.tank.beans
 
 import org.scalatest.FlatSpec
 import org.scalatest.matchers.ShouldMatchers
-import sk.kave.tank.helpers.{TestEventListener, GameTestContext}
-import sk.kave.tank.events.TankEvent
+import sk.kave.tank.helpers.{EventOccurredException, TestEventListener, GameTestContext}
+import sk.kave.tank._
+import events.{TankEvent, TankRotationEvent}
+import fx.UP
+import scala.Some
 
 /**
  * @autor : vilo
@@ -13,11 +16,22 @@ class TankImplTest extends FlatSpec with ShouldMatchers {
   implicit val gTestContext = new GameTestContext
 
   "A tank " should " change direction and then fires event " in {
-    implicit val gTestContext = new GameTestContext {
-      // not necessary to wrap tank in gContext,. I can test it directly,.. it'll rewrite
-      override lazy val tank = new TankImpl(10,10) with TestEventListener[TankEvent]
+    val direction : Vector2D = (None, Some(UP))
+    val newDirection : Vector2D = (None, Some(UP))
+    val tank = new TankImpl(10,10, direction) with TestEventListener[TankEvent]
+    val callback = () => println("nic")
 
+    val eventEx : EventOccurredException[TankEvent] = intercept[EventOccurredException[TankEvent]] {
+      tank.changeDirection( newDirection)(callback)
     }
+    tank.direction should equal (newDirection)
+
+    assert( eventEx.event.isInstanceOf[ TankRotationEvent] )
+    val tankEvent = eventEx.event.asInstanceOf[ TankRotationEvent]
+
+    tankEvent.newDirection should equal ( newDirection)
+    tankEvent.oldDirection should equal ( direction)
+    tankEvent.callback should equal ( callback)
   }
 
 }
--- src/test/scala/sk/kave/tank/helpers/TestEventListener.scala
+++ src/test/scala/sk/kave/tank/helpers/TestEventListener.scala
@@ -3,11 +3,12 @@ package sk.kave.tank.helpers
 import sk.kave.tank.events.{Event, EventListener}
 
 /**
- * @autor : wilo
+ * @autor : vilo
  */
-trait TestEventListener[E <: Event] extends EventListener[E] {
 
-  class EventOccurredException(event : E) extends Exception
+class EventOccurredException[E <: Event](val event : E) extends Exception
+
+trait TestEventListener[E <: Event] extends EventListener[E] {
 
   override def fireEvent(event: E) = throw new EventOccurredException(event)
 
