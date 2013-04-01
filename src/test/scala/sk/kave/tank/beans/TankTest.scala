8e113e48f3ea94b741236e1e30170deb80e49de7
Igor Kvasnicka <igor.kvasnicka@gmail.com>
2013-04-01 19:03:55 +0200

    TankTest

--- src/test/scala/sk/kave/tank/beans/TankTest.scala
+++ src/test/scala/sk/kave/tank/beans/TankTest.scala
@@ -1,12 +1,11 @@
 package sk.kave.tank.beans
 
-import org.scalatest.matchers.ShouldMatchers
 import org.scalatest._
 import junit.JUnitRunner
 import org.junit.runner.RunWith
 import scala.Predef._
 import sk.kave.tank._
-import fx.{RIGHT, LEFT, UP}
+import fx.{DOWN, RIGHT, LEFT, UP}
 import helpers.GameTestContext
 import scala.Some
 
@@ -21,35 +20,54 @@ class TankTest extends FlatSpec with ShouldMatchers {
 
   "A tank " should " calculate shortest rotation" in {
     import scala.collection.immutable.Map
-    val testData : Map[ (Vector2D, Vector2D), Int] = Map(
+    val testData: Map[(Vector2D, Vector2D), Int] = Map(
 
       ((None, Some(UP)), (Some(LEFT), Some(UP))) -> -45,
-      ((Some(LEFT), Some(UP)), (None, Some(UP))) -> 45 ,
-      ((None, Some(UP)), (Some(LEFT), None))     -> -90,
-      ((Some(LEFT), None), (None, Some(UP)))     -> 90 ,
+      ((Some(LEFT), Some(UP)), (None, Some(UP))) -> 45,
+      ((None, Some(UP)), (Some(LEFT), None)) -> -90,
+      ((Some(LEFT), None), (None, Some(UP))) -> 90,
 
-      ((None, Some(UP)), (Some(RIGHT), Some(UP)))-> 45 ,
-      ((Some(RIGHT), Some(UP)), (None, Some(UP)))-> -45,
-      ((None, Some(UP)), (Some(RIGHT), None))    -> 90 ,
-      ((Some(RIGHT), None), (None, Some(UP)))    -> -90
+      ((None, Some(UP)), (Some(RIGHT), Some(UP))) -> 45,
+      ((Some(RIGHT), Some(UP)), (None, Some(UP))) -> -45,
+      ((None, Some(UP)), (Some(RIGHT), None)) -> 90,
+      ((Some(RIGHT), None), (None, Some(UP))) -> -90,
+      ((Some(LEFT), Some(UP)), (Some(RIGHT), Some(DOWN))) -> -180
     )
 
-    for ( d <- testData.keys) {
-       val (f,t) = d
-       Tank.getAngle(f, t).toInt should equal(testData(d))
+    for (d <- testData.keys) {
+      val (f, t) = d
+      Tank.getAngle(f, t).toInt should equal(testData(d))
     }
   }
 
   it should " calculate full rotation " in {
-     //todo: Tank.getFullAngle
+    import scala.collection.immutable.Map
+    val testData: Map[(Vector2D, Vector2D), Int] = Map(
+
+      ((None, Some(UP)), (Some(LEFT), Some(UP))) -> -45,
+      ((Some(LEFT), Some(UP)), (None, Some(UP))) -> 45,
+      ((None, Some(UP)), (Some(LEFT), None)) -> -90,
+      ((Some(LEFT), None), (None, Some(UP))) -> 90,
+
+      ((None, Some(UP)), (Some(RIGHT), Some(UP))) -> -315,
+      ((Some(RIGHT), Some(UP)), (None, Some(UP))) -> 315,
+      ((None, Some(UP)), (Some(RIGHT), None)) -> -270,
+      ((Some(RIGHT), None), (None, Some(UP))) -> 270,
+      ((Some(LEFT), Some(UP)), (Some(RIGHT), Some(DOWN))) -> -180
+    )
+
+    for (d <- testData.keys) {
+      val (f, t) = d
+      Tank.getAngleFull(f, t).toInt should equal(testData(d))
+    }
   }
 
   it should " inspect if it is in position" in {
-    Tank.isInPosition(5, 5)  (gTestContext) should equal ((true,  true))
-    Tank.isInPosition(799, 5)(gTestContext) should equal ((false, true))
-    Tank.isInPosition(2, 5)  (gTestContext) should equal ((false, true))
-    Tank.isInPosition(5, 1)  (gTestContext) should equal ((true, false))
-    Tank.isInPosition(5, 799)(gTestContext) should equal ((true, false))
+    Tank.isInPosition(5, 5)(gTestContext) should equal((true, true))
+    Tank.isInPosition(799, 5)(gTestContext) should equal((false, true))
+    Tank.isInPosition(2, 5)(gTestContext) should equal((false, true))
+    Tank.isInPosition(5, 1)(gTestContext) should equal((true, false))
+    Tank.isInPosition(5, 799)(gTestContext) should equal((true, false))
   }
 
 }
